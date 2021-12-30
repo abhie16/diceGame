1f35bf556c540aaa8a64e9b13d2c7c9c07042735
Abhishek pandey <95357648+abhie16@users.noreply.github.com>
2021-12-30 23:36:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1= Math.floor( Math.random() * 6 ) + 1;
+var randomDiceImg="dice"+randomNumber1+".png";
+var randomImgSrc="images/"+randomDiceImg;
+var img1=document.querySelectorAll("img")[0];
+img1.setAttribute("src", randomImgSrc);
+
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomDiceImg1="dice"+randomNumber2+".png";
+var randomImgSrc1="images/"+randomDiceImg1;
+var img2=document.querySelectorAll("img")[1];
+img2.setAttribute("src", randomImgSrc1);
+
+
+if (randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩 Player 1 Wins!!";
+}
+else if (randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩🚩 Player 2 Wins!!";
+}
+else
+{
+  document.querySelector("h1").innerHTML="It is a tie!!";
+}
