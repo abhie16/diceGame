6229aa3edbba563d8437aed124377aadf54a820a
Abhishek pandey <95357648+abhie16@users.noreply.github.com>
2021-12-31 00:12:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var randomNumber1= Math.floor( Math.random() * 6 ) + 1;
 var randomDiceImg="dice"+randomNumber1+".png";
-var randomImgSrc="images/"+randomDiceImg;
+var randomImgSrc=randomDiceImg;
 var img1=document.querySelectorAll("img")[0];
 img1.setAttribute("src", randomImgSrc);
 
 
 var randomNumber2= Math.floor(Math.random()*6)+1;
 var randomDiceImg1="dice"+randomNumber2+".png";
-var randomImgSrc1="images/"+randomDiceImg1;
+var randomImgSrc1=randomDiceImg1;
 var img2=document.querySelectorAll("img")[1];
 img2.setAttribute("src", randomImgSrc1);
 
